680934a365b10372f4bf686c3f5d92f6e04356c4
bushicherry <31837159+bushicherry@users.noreply.github.com>
2018-12-02 12:18:51 -0500

    Initialize

---
+++ src/java/Driver.java
@@ -0,0 +1,233 @@
+package java;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.*;
+
+public class Driver {
+
+    public static void main(String[] args) throws SocketException {
+        Scanner hostsInput;
+        int numHosts=0;
+        HashMap<String, int[] > hostsPorts = new HashMap<>();
+
+        // attempts to create scanner for 'knownhosts_udp.txt'
+        try
+        {
+            hostsInput = new Scanner(new File("knownhosts_udp.txt"));
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File 'knownhosts_udp.txt' doesn't exist.");
+            return;
+        }
+
+        // reads the file line by line
+        while(hostsInput.hasNextLine()) {
+            numHosts = numHosts+1;
+            int[] intArr = new int[2];
+            String tmpName = hostsInput.next();
+            intArr[0] = Integer.parseInt(hostsInput.next());
+            intArr[1] = numHosts;
+            hostsPorts.put(tmpName,intArr);
+        }
+
+        // test output the map
+        /*for (HashMap.Entry<String,Integer> entry : hostsPorts.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue() + "\n");
+        }*/
+
+        final String myName = args[0];
+        final int myPort = hostsPorts.get(myName)[0];
+        final int myIndex = hostsPorts.get(myName)[1];
+        final int numOfHosts = numHosts;
+        // set up log and dictionary
+        final LogAndDic logAndDic = new LogAndDic(numHosts,myIndex);
+        LogAndDic.sendPac[] recov = Algorithm.reload_pro(numOfHosts);
+        logAndDic.read_DicAlog_pac(recov[0], recov[1]);
+        final DatagramSocket socket = new DatagramSocket(myPort);
+
+
+        // set up udp socket on a new thread to listen for msgs from other sites
+        Runnable udpListener = new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(myName + ": start listening for msgs.");
+                while (true) {
+                    byte[] buffer = new byte[65507];
+                    DatagramPacket packet = new DatagramPacket(buffer,0,buffer.length);
+                    try {
+                        socket.receive(packet);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+
+                    Runnable recvMsg = new Runnable() {
+                        @Override
+                        public synchronized void run() {
+
+                            // byte to sendPac
+                            LogAndDic.sendPac pac = Algorithm.byte2sendPac(buffer,numOfHosts);
+//                            byte[] tmp = Arrays.copyOfRange(buffer,0,64);
+//                            String msg_recv = new String(tmp).trim();
+//                            int index_recv = buffer[64];
+//                            int[][] Ti_recv = new int[numOfHosts][numOfHosts];
+//                            for (int i=0; i<numOfHosts; i++) {
+//                                for (int j=0; j<numOfHosts; j++) {
+//                                    Ti_recv[i][j] = buffer[65+i*numOfHosts+j];
+//                                }
+//                            }
+//                            Vector<LogAndDic.eRecord> NP_recv = new Vector<>();
+//
+//                            int offset = 65+numOfHosts*numOfHosts;
+//                            byte comma = ',';
+//                            while (buffer[offset]==comma) {
+//                                int ts_recv = buffer[offset+1];
+//                                ts_recv = ts_recv*128+buffer[offset+2];
+//
+//                                int Pi_recv = buffer[offset+3];
+//
+//                                tmp = Arrays.copyOfRange(buffer,offset+4,offset+24);
+//                                String name_recv = new String(tmp).trim();
+//
+//                                meetingInfo op_recv;
+//
+//                                if (buffer[offset+24]!=(byte)0) {
+//                                    int month_recv = buffer[offset+24];
+//                                    int day_recv = buffer[offset+25];
+//                                    int year_recv = buffer[offset+26];
+//                                    year_recv = year_recv*128+buffer[offset+27];
+//                                    int shour_recv = buffer[offset+28];
+//                                    int smin_recv = buffer[offset+29];
+//                                    int ehour_recv = buffer[offset+30];
+//                                    int emin_recv = buffer[offset+31];
+//
+//                                    offset = offset+32;
+//                                    Vector<String> users_recv = new Vector<>();
+//                                    while (buffer[offset]==comma) {
+//                                        tmp = Arrays.copyOfRange(buffer,offset+1,offset+26);
+//                                        users_recv.add(new String(tmp).trim());
+//                                        offset += 26;
+//                                    }
+//
+//                                    op_recv = new meetingInfo(name_recv,
+//                                            new GregorianCalendar(year_recv, month_recv, day_recv),
+//                                            new GregorianCalendar(year_recv, month_recv, day_recv, shour_recv, smin_recv),
+//                                            new GregorianCalendar(year_recv, month_recv, day_recv, ehour_recv, emin_recv),
+//                                            users_recv);
+//                                    offset++;
+//                                } else {
+//                                    op_recv = new meetingInfo(name_recv);
+//                                    offset += 25;
+//                                }
+//
+//                                NP_recv.add(new LogAndDic.eRecord(op_recv, ts_recv, Pi_recv));
+//                            }
+//                            LogAndDic.sendPac pac = new LogAndDic.sendPac(msg_recv,Ti_recv,NP_recv,index_recv);
+
+                            Algorithm.Onrec(logAndDic, pac, myName, hostsPorts, socket);
+                        }
+                    };
+
+                    new Thread(recvMsg).start();
+                }
+            }
+        };
+
+        new Thread(udpListener).start();
+
+
+        // loop to deal with commands
+        while (true) {
+            Scanner commandS = new Scanner(System.in);
+            String command;
+
+            command = commandS.next();
+            if (command.equals("schedule")) {
+                String name = commandS.next();
+
+                String dateStr = commandS.next();
+                String pattern = "MM/dd/yyyy";
+                Calendar date = Calendar.getInstance();
+                SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.ENGLISH);
+                try {
+                    date.setTime(sdf.parse(dateStr));// all done
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    continue;
+                }
+
+                String startStr =  commandS.next();
+                String endStr = commandS.next();
+                LocalTime startTime;
+                LocalTime endTime;
+                try {
+                    startTime = LocalTime.parse(startStr);
+                    endTime = LocalTime.parse(endStr);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    continue;
+                }
+                // check if the times are in 30 minutes increments
+                if (startTime.getMinute()%30 != 0 || endTime.getMinute()%30 != 0) {
+                    System.out.println("Unable to schedule meeting " + name +".");
+                    continue;
+                } else if( startTime.isAfter(endTime) ){
+                    System.out.println("Unable to schedule meeting <Start time after end time>" + name +".");
+                    continue;
+                }
+
+                Vector<String> participants = new Vector<>();
+                boolean selfIncluded = false;
+                String users = commandS.next();
+                String[] userArray = users.split(",");
+                for (String u : userArray) {
+                    if (u.equals(myName)) selfIncluded = true;
+                    participants.add(u);
+                }
+                if (!selfIncluded) {
+                    System.out.println("Unable to schedule meeting " + name +".");
+                    continue;
+                }
+
+                GregorianCalendar gDate = new GregorianCalendar(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE));
+                GregorianCalendar gStartTime = new GregorianCalendar(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE), startTime.getHour(), startTime.getMinute());
+                GregorianCalendar gEndTime = new GregorianCalendar(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE), endTime.getHour(), endTime.getMinute());
+                meetingInfo m = new meetingInfo(name, gDate, gStartTime, gEndTime, participants);
+
+                Algorithm.Insert(logAndDic, m, hostsPorts, myName, socket);
+
+
+
+            }
+            else if (command.equals("cancel")) {
+                String name = commandS.next();
+                Algorithm.Cancel(logAndDic,name,myName,hostsPorts,socket);
+            }
+            else if (command.equals("view")) {
+                logAndDic.View_dic();
+            }
+            else if (command.equals("myview")) {
+                logAndDic.myView(myName);
+            }
+            else if (command.equals("log")) {
+                logAndDic.View_log();
+            }
+            else {
+                System.out.println("The command is not recognizable. Please follow the following formats:\n" +
+                        "schedule <name> <day> <start_time> <end_time> <participants>\n" +
+                        "cancel <name>\nview\nmyview\nlog");
+            }
+        }
+    }
+
+}
---
+++ src/java/meetingInfo.java
@@ -0,0 +1,61 @@
+package java;
+
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Vector;
+
+public class meetingInfo {
+    // name of the meeting
+    public String name;
+    // day and time
+    public GregorianCalendar day;
+    public GregorianCalendar start;
+    public GregorianCalendar end;
+    // users
+    public Vector<String> users;
+    //indicator 1 if insert, 0 if delete
+
+    // constructor for deletion
+    public meetingInfo(String n){
+        name = n;
+        users = null;
+        day = null;
+        start = null;
+        end = null;
+    }
+    // constructor
+    public meetingInfo(String n, GregorianCalendar d, GregorianCalendar s, GregorianCalendar e, Vector<String> u){
+        name = n;
+        day = new GregorianCalendar(d.get(Calendar.YEAR), d.get(Calendar.MONTH), d.get(Calendar.DATE));
+        start = new GregorianCalendar(d.get(Calendar.YEAR), d.get(Calendar.MONTH), d.get(Calendar.DATE),s.get(Calendar.HOUR_OF_DAY), s.get(Calendar.MINUTE));
+        end = new GregorianCalendar(d.get(Calendar.YEAR), d.get(Calendar.MONTH), d.get(Calendar.DATE),e.get(Calendar.HOUR_OF_DAY), e.get(Calendar.MINUTE));
+        users = new Vector<>(u);
+    }
+
+    public meetingInfo(meetingInfo MI){
+        if(MI.users != null) {
+            name = MI.name;
+            day = new GregorianCalendar(MI.day.get(Calendar.YEAR), MI.day.get(Calendar.MONTH), MI.day.get(Calendar.DATE));
+            start = new GregorianCalendar(MI.day.get(Calendar.YEAR), MI.day.get(Calendar.MONTH), MI.day.get(Calendar.DATE), MI.start.get(Calendar.HOUR_OF_DAY), MI.start.get(Calendar.MINUTE));
+            end = new GregorianCalendar(MI.day.get(Calendar.YEAR), MI.day.get(Calendar.MONTH), MI.day.get(Calendar.DATE), MI.end.get(Calendar.HOUR_OF_DAY), MI.end.get(Calendar.MINUTE));
+            users = new Vector<>(MI.users);
+        } else {
+            name = MI.name;
+            users = null;
+            day = null;
+            start = null;
+            end = null;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof meetingInfo) {
+            meetingInfo m = (meetingInfo)o;
+            return name.equals(m.name); //Given that name of each meeting is unique
+        }
+        return false;
+    }
+
+}
