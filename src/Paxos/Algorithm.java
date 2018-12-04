9b4b2e775b286f318f12000564e8959cfc11db10
dp540788912 <lgx540788912@gmail.com>
2018-12-03 23:18:45 -0500

    update driver

--- src/Paxos/Algorithm.java
+++ src/Paxos/Algorithm.java
@@ -1,11 +1,16 @@
 package Paxos;
 
+import java.net.DatagramSocket;
+import java.util.HashMap;
+
 public class Algorithm {
     /**
      * On receiving message
      * monitor the socket. After receiving packet from other node
      * Prepare response
      */
+    public void OnRecv(PaxosLog Ploh, Dictionary Dic, Packet RecvPacket, String myname, HashMap<String, int[] > HashPorts, DatagramSocket socket){
 
+    }
 
 }
