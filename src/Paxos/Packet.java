1798f62c41722f885ecc34d59a0ede72e1776dc8
dp540788912 <lgx540788912@gmail.com>
2018-12-03 22:18:51 -0500

    update driver

--- src/Paxos/Packet.java
+++ src/Paxos/Packet.java
@@ -10,6 +10,7 @@ public class Packet implements Serializable {
     public int accNum; // actual number each process
     public meetingInfo accValue; // actual value
     public int packetType; // the packet type
+                    // -1 for no type
                     // 0 for prepare packet
                     // 1 for prepare response packet
                     // 2 for accept packet
@@ -17,24 +18,23 @@ public class Packet implements Serializable {
                     // 4 for commit packet
                     // 5 for nack packet
                     // 6 for asking for missing holes
+                    //
     public int LogIndex; // LogEntry index
 
 
     // for filling the hole
-    public int StartNumOfLogIndex; // the start index for missing LogEntry
-    public int EndNumOfLogIndex; // the end index for missing LogEntry
+    public int[] missingIndexList;
     public ArrayList<PaxosLog.LogEntry> RespLogArray; // responded LogEntry array
 
 
-    public Packet(int pn, int an, meetingInfo av, int pt,int ln, int StarNumberOfIndex_, int EndNumOfIndex_, ArrayList<PaxosLog.LogEntry> LogArray){
+    public Packet(int pn, int an, meetingInfo av, int pt,int ln, int[]_missingIndexList, ArrayList<PaxosLog.LogEntry> LogArray){
         this.propNUm = pn;
         this.accNum = an;
         this.accValue = av;
         this.packetType = pt;
         this.LogIndex = ln;
 
-        this.StartNumOfLogIndex = StarNumberOfIndex_;
-        this.EndNumOfLogIndex = EndNumOfIndex_;
+        this.missingIndexList = _missingIndexList;
         this.RespLogArray = LogArray;
 
     }
--- src/Paxos/PaxosLog.java
+++ src/Paxos/PaxosLog.java
@@ -2,21 +2,25 @@ package Paxos;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class PaxosLog implements Serializable {
     // a LogEntry array to store
     private ArrayList<LogEntry> repLog; // replicated LogEntry for each site
     private int lastPropNum; // the last used proposal number
+    private Vector<PaxosLog.LogEntry> EmptyLog; // indicate if there are any holes
 
     // constructor
     public PaxosLog(){
         this.repLog = new ArrayList<>();
         this.lastPropNum = 0;
+        EmptyLog = new Vector<>();
     }
 
     public PaxosLog(PaxosLog pLog){
         this.repLog = pLog.getRepLog();
         this.lastPropNum = pLog.getLastPropNum();
+        EmptyLog = pLog.getEmptyLog();
     }
 
     // get
@@ -28,6 +32,10 @@ public class PaxosLog implements Serializable {
         return lastPropNum;
     }
 
+    public Vector<PaxosLog.LogEntry> getEmptyLog() {
+        return EmptyLog;
+    }
+
     public void setLastProNum(int newNum){
         this.lastPropNum = newNum;
     }
@@ -35,7 +43,29 @@ public class PaxosLog implements Serializable {
     // insert an LogEntry
     public void insertLog(LogEntry l){ // l is a new LogEntry
         this.repLog.add(l);
+        if(l.getMeeting() == null){
+            this.EmptyLog.add(l);
+        }
     }
+
+    // insert a value to empty hole
+    public boolean insertEmptyLog(int Index, meetingInfo value){
+        if(repLog.get(Index-1).getMeeting() != null){
+            System.out.println("Not a hole, but a value is intended to be inserted");
+            return false;
+        }
+        this.repLog.get(Index -1).setMeeting(value);
+        this.EmptyLog.remove(repLog.get(Index-1));
+        return true;
+    }
+
+
+    // check if there is any empty hole exist
+    public boolean IfHoleExist(){
+        if (EmptyLog.size() == 0) return false;
+        else return true;
+    }
+    // check if
 
 
     //print_out LogEntry
@@ -43,7 +73,6 @@ public class PaxosLog implements Serializable {
         for(LogEntry l: repLog){
             if (l.getMeeting().getUser() ==null){
                 System.out.println("Cancel " + l.getMeeting().getName());
-
             } else {
                 System.out.print("Schedule " + l.getMeeting().toString());
             }
@@ -65,6 +94,7 @@ public class PaxosLog implements Serializable {
             this.LogIndex = LgIndex;
         }
 
+
         public meetingInfo getMeeting(){
             return this.meeting;
         }
@@ -75,6 +105,10 @@ public class PaxosLog implements Serializable {
 
         public int getType(){
             return this.type;
+        }
+
+        public void setMeeting(meetingInfo m) {
+            meeting = m;
         }
     }
 
